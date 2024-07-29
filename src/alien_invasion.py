d266c5feefca111d6ba1f9e187b557f2cea6fd07
Ramziddin Suyunov <ramziddinsuyunov@usf.edu>
2024-07-28 22:02:49 -0700

    Add Ship class with the bmp image of the ship

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -1,16 +1,20 @@
 import sys
+
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
         
 
     def run_game(self):
@@ -21,6 +25,9 @@ class AlienInvasion:
                     sys.exit()
 
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            #Make the most recently drawn screen visible
             pygame.display.flip()
             self.clock.tick(60)
 
