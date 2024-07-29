0b51a87497a8e171af2fe74ec862a698886ecdef
Ramziddin Suyunov <ramziddinsuyunov@usf.edu>
2024-07-28 23:29:14 -0700

    Add moving right and left at 1.5x speed feature

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -20,16 +20,34 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            #Make the most recently drawn screen visible
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keypress and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True 
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Updates images on the screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        #Make the most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- src/settings.py
+++ src/settings.py
@@ -6,5 +6,6 @@ class Settings:
 
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (100, 100, 200)
+        self.ship_speed = 1.5
         
