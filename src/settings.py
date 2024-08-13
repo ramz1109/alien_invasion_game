8d6e260acca05fbe94e72f8e39fc23f96d022448
Ramziddin Suyunov <mrsuyunov1109@gmail.com>
2024-08-13 05:16:23 -0400

    Initialize bullet.py

--- src/settings.py
+++ src/settings.py
@@ -9,3 +9,8 @@ class Settings:
         self.bg_color = (100, 100, 200)
         self.ship_speed = 3
         
+        # Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_height = 3 
+        self.bullet_width = 15
+        self.bullet_color = (60, 60, 60)
