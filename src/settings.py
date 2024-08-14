f3f41ecb6f341e239aa6352d5339a4f2f81cf698
Ramziddin Suyunov <mrsuyunov1109@gmail.com>
2024-08-13 20:13:28 -0400

    Add firing bullets

--- src/settings.py
+++ src/settings.py
@@ -11,6 +11,6 @@ class Settings:
         
         # Bullet settings
         self.bullet_speed = 2.0
-        self.bullet_height = 3 
-        self.bullet_width = 15
+        self.bullet_height = 15
+        self.bullet_width = 3
         self.bullet_color = (60, 60, 60)
