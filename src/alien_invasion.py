00d98737fa62ce8ed24f06bb61daa0e5f320085f
Ramziddin Suyunov <mrsuyunov1109@gmail.com>
2024-08-14 23:33:03 -0400

    Add alien fleet

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -5,20 +5,26 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien 
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
+
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width 
         self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
         
 
     def run_game(self):
@@ -26,14 +32,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove()
-            print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
@@ -53,6 +52,7 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         #Make the most recently drawn screen visible
         pygame.display.flip()
@@ -85,8 +85,35 @@ class AlienInvasion:
     
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update the position of the bullets and get rid of old bullets"""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # Create an alien and keep adding aliens until there's no room left.
+        # Spacing between aliens is one alien width.
+        alien = Alien(self)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self) 
+            new_alien.x = current_x 
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- src/settings.py
+++ src/settings.py
@@ -6,7 +6,7 @@ class Settings:
 
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (100, 100, 200)
+        self.bg_color = (230, 230, 230)
         self.ship_speed = 3
         
         # Bullet settings
@@ -14,3 +14,4 @@ class Settings:
         self.bullet_height = 15
         self.bullet_width = 3
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
