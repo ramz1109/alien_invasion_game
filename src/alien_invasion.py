d65d21b28910b06bc570ce166a02c93f652b2945
Ramziddin Suyunov <ramziddinsuyunov@Ramziddins-Air.lan>
2024-08-22 20:21:52 -0400

    moving fleet

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -33,6 +33,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -124,7 +125,24 @@ class AlienInvasion:
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet"""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed 
+        self.settings.fleet_direction *= -1
 
 if __name__ == '__main__':
     ai = AlienInvasion()
-    ai.run_game()
+    ai.run_game()   
--- src/settings.py
+++ src/settings.py
@@ -15,3 +15,11 @@ class Settings:
         self.bullet_width = 3
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        #Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_directio of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+        
