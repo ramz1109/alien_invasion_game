ded057d706045ee9b204a34d8a1f8eff28b1267c
Ramziddin Suyunov <ramziddinsuyunov@Ramziddins-Air.lan>
2024-08-23 21:52:54 -0400

    Adding score board

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -23,8 +24,9 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
 
-        #Create an instance to store game statistics.
+        #Create an instance to store game statistics and create a scoreboard.
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -72,6 +74,9 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        #Draw the score information.
+        self.sb.show_score()
 
         # Draw the play button if the game is inactive.
         if not self.game_active:
@@ -135,6 +140,7 @@ class AlienInvasion:
             #Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -217,8 +223,9 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
-        if self.play_button.rect.collidepoint(mouse_pos):
-            # Reset the game statistics.
+        if button_clicked and not self.game_active:
+            # Reset the game statistics
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.game_active = True
             
